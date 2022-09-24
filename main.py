e88455f84a706f2ed6c669931abfad1b8be170b0
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-09-24 10:34:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import mean
+a=(2,2,6,1,1)
+mean(a)
+
+myNumber: 447.125
+myRoundNumber: round(447.13)
+
+username= int(input(123))
+password= input(goiko)
+if nombre==231 and password==goiko:
+  print(231GoikoCorrecto)
+else:
+  print(AcessoDenegado)
+
+a=int(3)
+print(a)
+b=int(2)
+print(b)
+int(a+1)
+print(4)
+int(b-2)
+print(0)
+int((a+1)*3)
+print(12)
+int((b-2)/2)
+print(0)
+A=12
+B=0
